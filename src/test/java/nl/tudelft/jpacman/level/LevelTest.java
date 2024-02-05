9dcaa3447da4c27a1d69ef1737f2717a8c7149c6
Michelle McGowan <mcgowm1@unlv.nevada.edu>
2024-02-04 17:31:47 -0800

    Fixes
    - code cleanup to test methods

--- src/test/java/nl/tudelft/jpacman/level/LevelTest.java
+++ src/test/java/nl/tudelft/jpacman/level/LevelTest.java
@@ -6,6 +6,7 @@ import nl.tudelft.jpacman.npc.Ghost;
 import nl.tudelft.jpacman.npc.ghost.GhostFactory;
 import nl.tudelft.jpacman.points.PointCalculator;
 import nl.tudelft.jpacman.sprite.PacManSprites;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -17,34 +18,38 @@ import static org.mockito.Mockito.mock;
 public class LevelTest {
 
     private static final PacManSprites sprites = new PacManSprites();
-    private static final GhostFactory ghost = new GhostFactory(sprites);
+    private static final GhostFactory ghostFactory = new GhostFactory(sprites);
+    private static final Ghost ghost = mock(Ghost.class);
 
     private static final PointCalculator point = mock(PointCalculator.class);
-    public LevelFactory levelFactory = new LevelFactory(sprites, ghost, point);
-    static List<Ghost> ghosts =  new ArrayList<>();
-    static List<Square> startPositions = new ArrayList<>();
+    public LevelFactory levelFactory = new LevelFactory(sprites, ghostFactory, point);
+    static List<Ghost> ghostList =  new ArrayList<>();
+    static List<Square> startPositionsList = new ArrayList<>();
+    private static final Board board = mock(Board.class);
+    private static final Square startPos1 = mock(Square.class);
+    private static final CollisionMap cmap = mock(CollisionMap.class);
 
     @Test
     void testisInProgress() {
-        Board board = mock(Board.class);
-        ghosts.add(ghost.createClyde());
-        ghosts.add(ghost.createBlinky());
-        ghosts.add(ghost.createInky());
-        ghosts.add(ghost.createPinky());
-        startPositions.add(board.squareAt(0,0));
-        Level level = levelFactory.createLevel(board, ghosts, startPositions);
-        assertThat(level.isInProgress()).isEqualTo(false);
+        // get the level
+        // pass in the board, ghost list, starting positions, collision map
+        Level level = new Level(board, Lists.newArrayList(ghost), Lists.newArrayList(startPos1), cmap);
+        // start the level
+        level.start();
+        assertThat(level.isInProgress()).isEqualTo(true);
     }
 
     @Test
     void getBoard() {
-        Board board = mock(Board.class);
-        ghosts.add(ghost.createClyde());
-        ghosts.add(ghost.createBlinky());
-        ghosts.add(ghost.createInky());
-        ghosts.add(ghost.createPinky());
-        startPositions.add(board.squareAt(0,0));
-        Level level = levelFactory.createLevel(board, ghosts, startPositions);
+        // create ghosts for the game
+        ghostList.add(ghostFactory.createClyde());
+        ghostList.add(ghostFactory.createBlinky());
+        ghostList.add(ghostFactory.createInky());
+        ghostList.add(ghostFactory.createPinky());
+        // starting position
+        startPositionsList.add(board.squareAt(0, 0));
+        // get the level
+        Level level = levelFactory.createLevel(board, ghostList, startPositionsList);
         assertThat(level.getBoard()).isNotNull();
     }
 
